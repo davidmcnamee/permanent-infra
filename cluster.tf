599d199a945e75cd7c01a629c103c972f21e22fe
David McNamee <davidmcnamee314@gmail.com>
2021-08-05 04:21:01 -0400

    kubectl working

--- cluster.tf
+++ cluster.tf
@@ -13,6 +13,12 @@ resource "aws_security_group" "ec2_security_group" {
   name        = "cluster-ec2-security-group"
   vpc_id      = aws_vpc.primary.id
   ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
