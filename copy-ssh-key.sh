ad5d5376e643a3b4443fa79e19155dc006b49732
David McNamee <davidmcnamee314@gmail.com>
2021-09-10 21:54:00 -0400

    add retries to scp

--- copy-ssh-key.sh
+++ copy-ssh-key.sh
@@ -2,6 +2,13 @@
 JSON_INPUT=`cat`
 IP=$(echo $JSON_INPUT | sed 's/.*"ip"\: *"\([^ ]*\)".*/\1/')
 python3 ./update-ssh-config.py $IP
-sleep 60 # ssh not available immediately after server provision
-scp -o StrictHostKeyChecking=accept-new ~/.ssh/id_rsa davidmcnamee@$IP:~/.ssh/id_rsa &> /dev/null
+max_tries=40
+status_code=1
+while [[ $status_code == 1 && $max_tries > 0 ]]; do
+    max_tries=$((max_tries-1))
+    scp -o StrictHostKeyChecking=accept-new ~/.ssh/id_rsa davidmcnamee@$IP:~/.ssh/id_rsa &> /dev/null
+    status_code=$?
+    sleep 5
+done
+if [[ $status_code == 1 ]]; then exit 1; fi
 echo "{}"
