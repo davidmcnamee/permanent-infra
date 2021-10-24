0c3eb518f13650f8ac381c419f15f3fbec380855
David McNamee <davidmcnamee314@gmail.com>
2021-10-23 22:25:21 -0400

    remove dev/gke dependency

--- dev-environment.tf
+++ dev-environment.tf
@@ -33,7 +33,7 @@ resource "google_compute_instance" "dev_server" {
         gh repo list -L 7 --json sshUrl | jq -r ".[] | .sshUrl" | while read repo; do git clone $repo; done
         sudo snap install --classic google-cloud-sdk &>> ~/brew-install.log
         gcloud auth activate-service-account --key-file=$HOME/.config/gcloud/application_default_credentials.json &>> ~/brew-install.log
-        echo 'alias gke-creds="gcloud container clusters get-credentials ${google_container_cluster.cluster.name} --region us-central1"' >> ~/.bashrc
+        echo 'alias gke-creds="gcloud container clusters get-credentials ${local.gke_username} --region us-central1"' >> ~/.bashrc
         sudo apt install awscli -y &>> ~/brew-install.log
         brew install node yarn python go rustup docker minikube skaffold java bazelisk argocd tree helm terraform &>> ~/brew-install.log
         git config --global pull.rebase true
